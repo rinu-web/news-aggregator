40491642e365fba92f627a4c9c6322abab7b82cb
rinu-web <rinuraji44@gmail.com>
2025-03-11 16:25:16 +0530

    Added validation for login and password

--- app/Http/Requests/PasswordResetRequest.php
+++ app/Http/Requests/PasswordResetRequest.php
@@ -22,7 +22,7 @@ class PasswordResetRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+             'email' => 'required|email'
         ];
     }
 }
